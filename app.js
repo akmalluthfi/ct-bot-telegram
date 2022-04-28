e85874ad495b7ae7b0b863303b07615b86149d73
akmalluthfi <akmalluthfi19@gmail.com>
2022-04-28 10:45:26 +0700

    add new command

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const { Telegraf } = require('telegraf');
 const token = process.env.TOKEN;
 
-// console.log(process.env.TOKEN);
 const bot = new Telegraf(token);
 
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.command('name', (ctx) => ctx.reply('Akmal Luthfi'));
-bot.command('age', (ctx) => ctx.reply('i am 17 yo'));
-bot.command('where', (ctx) => ctx.reply('i live in surabaya, indonesian'));
-bot.command('email', (ctx) => ctx.reply('my email is akmalluthfi19@gmail.com'));
+// Global Commands
+bot.start((ctx) =>
+  ctx.reply(
+    'Welcome to my bot, This bot will help you to recognize who is akmal'
+  )
+);
+bot.help((ctx) =>
+  ctx.reply('just write "/" to find out what commands are available')
+);
 
-bot.command('test', async (ctx) => {
-  console.log('get chat');
-  console.log('==========');
-  const a = await ctx.getChat();
-  console.log(a.location);
-
-  console.log('me');
-  console.log('==========');
-  console.log(ctx.message.author_signature);
-
-  console.log('chat member');
-  console.log('==========');
-  console.log(ctx.id);
-
-  ctx.reply('wait...');
+bot.command('name', (ctx) =>
+  ctx.reply('Hi, my name is Akmal Luthfi, you can call me akmal')
+);
+bot.command('birth', (ctx) =>
+  ctx.reply(' I was born in Surabaya, on December 19th, 2004.')
+);
+bot.command('age', (ctx) => ctx.reply('I am 17 years old.'));
+bot.command('where', (ctx) =>
+  ctx.reply('Currently, I live in Surabaya, East Java - Indonesia.')
+);
+bot.command('email', (ctx) =>
+  ctx.reply('my email is akmalluthfi19@gmail.com.')
+);
+bot.command('secret', (ctx) => {
+  console.log(ctx);
 });
 
 bot.launch();
