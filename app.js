97b49ac4cb4323e3f401a6245a9fff3b8cce8a72
akmalluthfi <akmalluthfi19@gmail.com>
2022-04-28 13:16:34 +0700

    menghapus route GET /

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 const { Telegraf, Markup } = require('telegraf');
 const cool = require('cool-ascii-faces');
 const token =
